03a433eb12c991189765512e80e69cb9d399b095
Anton Marijanovic <anton_m00@hotmail.com>
2023-12-06 09:41:26 +0100

    fixed flex=

---
+++ lektion-5/index.js
@@ -0,0 +1,14 @@
+// const number = 23478
+// const string = 'halo'
+// const bool = true 
+// const undefinedValue = undefined
+// const nullValue = null
+
+// const arrValue = []
+// const mapValue = new Map()
+// const objValue = {}
+// const setValue = new
+
+document.querySelector('.title')
+
+console.log(title);
