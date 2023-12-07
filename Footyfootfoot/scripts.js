8ca4b6ead6c8b6dfd564100b97c348ebc73d08f9
Anton Marijanovic <anton_m00@hotmail.com>
2023-12-07 17:24:33 +0100

    balance page +

--- Footyfootfoot/scripts.js
+++ Footyfootfoot/scripts.js
@@ -1,8 +1,30 @@
-// function myFunction() {
-//     var x = document.getElementById("myLinks");
-//     if (x.style.display === "block") {
-//         x.style.display = "none";
-//     } else {
-//         x.style.display = "block";
-//     }
-// }
+/*Balance Script*/ 
+
+function addToBalance(){
+    var enteredAmount = document.getElementById("mnyInput").value;
+
+    enteredAmount = parseFloat(enteredAmount);
+
+    if (!isNaN(enteredAmount)) {
+      var currentBalance = parseFloat(document.getElementById("accountBalance").innerText);
+      var newBalance = currentBalance + enteredAmount;
+      
+      document.getElementById("accountBalance").innerText = newBalance.toFixed(2);
+
+      localStorage.setItem("accountBalance", newBalance);
+
+      document.getElementById("mnyInput").value = '';
+    } else {
+      alert("Please enter a valid number.");
+    }
+  }
+
+  window.onload = function() {
+    var storedBalance = localStorage.getItem("accountBalance");
+    if (storedBalance !== null) {
+      document.getElementById("accountBalance").innerText = parseFloat(storedBalance).toFixed(2);
+    }
+  }
+
+/*---------------------------------------------------------------------------------------------------*/
+
